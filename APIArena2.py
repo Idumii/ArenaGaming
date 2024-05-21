4953b81362b8986d1c2fbd784014c73c20a2bac5
Idumi <vincent.dievart.8@gmail.com>
2024-05-21 22:25:21 +0200

    add file gitignore + APIArena2 Ajouts InGame check and announcement started

--- APIArena2.py
+++ APIArena2.py
@@ -17,6 +17,12 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 key = os.getenv('API_RIOT_KEY')
 
+# Charger la liste des invocateurs à surveiller à partir d'un fichier JSON
+try:
+    with open('summoners_to_watch.json', 'r', encoding='utf-8') as f:
+        summoners_to_watch = json.load(f)
+except FileNotFoundError:
+    summoners_to_watch = []
 
 @client.event
 async def on_ready():
@@ -25,23 +31,8 @@ async def on_ready():
         await tree.sync()
     except Exception as e:
         print(e)
+    check_summoners_status.start()
 
-#@tree.command()
-#@app_commands.describe(
-#    first_value='The first value you want to add something to',
-#    second_value='The value you want to add to the first value',
-#)
-#async def add(interaction: discord.Interaction, first_value: int, second_value: int):
-#    """Adds two numbers together."""
-#    await interaction.response.send_message(f'{first_value} + {second_value} = {first_value + second_value}')
-
-@tree.command(name='ping', description='Latence du bot')
-async def slashPing(ctx):
-    await ctx.response.send_message(f'Pong! {client.latency}!')    
-
-#@tree.command(name='hello')
-#async def hello(intrecation: discord.Integration):
-#    await intrecation.response.send_message(f'Coucou')
 def clearGamename(gamenameWithspaces):
     result = ""
     for n in gamenameWithspaces:
@@ -49,25 +40,22 @@ def clearGamename(gamenameWithspaces):
     return result
 
 def requestSummoner(name, tag):
-
     account_url = f'https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tag}?api_key={key}'
     account_response = requests.get(account_url)
-    
+
     if account_response.status_code == 404:
         print('Account N exsite pas')
         raise ValueError("Invocateur n'exsite pas")
-        
     elif account_response.status_code != 200:
         print('Erreur dans l obtention des donnes du compte')
         raise ValueError("Erreur lors de l'obtention des données")
-        
 
     account_data = account_response.json()
     puuid = account_data['puuid']
-    
+
     summoner_url = f'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}?api_key={key}'
     summoner_response = requests.get(summoner_url)
-    
+
     if summoner_response.status_code == 404:
         print('Invocateur N exsite pas')
         raise ValueError("Invocateur n'exsite pas")
@@ -76,9 +64,7 @@ def requestSummoner(name, tag):
         raise ValueError("Erreur lors de l'obtention des données")
 
     summoner_data = summoner_response.json()
-
     summonerId = summoner_data.get('id')
-
     summonerTagline = account_data.get('tagLine')
     summonerGamename = account_data.get('gameName')
     summonerLevel = "Lvl." + str(summoner_data['summonerLevel'])
@@ -88,9 +74,7 @@ def requestSummoner(name, tag):
     totalMastery_response = requests.get(totalMastery_url)
     totalMastery_data = totalMastery_response.json()
 
-
     return summonerTagline, summonerGamename, summonerLevel, profileIcon, summonerId, totalMastery_data, puuid
-
 
 def fetchRanks(summonerId):
     ranks_url = f'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/{summonerId}?api_key={key}'
@@ -104,11 +88,10 @@ def fetchRanks(summonerId):
             for j in range(6):
                 ranks.append(ranks_data[i][calls[j]])
     except:
-        pass 
-    
+        pass
+
     print(ranks)
     return ranks
-
 
 @tree.command(name='invocateur', description='Profil d\'Invocateur')
 @app_commands.describe(pseudo='Nom invocateur', tag='EUW')
@@ -124,9 +107,7 @@ async def invocateur(interaction: discord.Interaction, pseudo: str, tag: str):
             color=discord.Colour.gold()
         )
         embed.add_field(name='Score total de maitrise: ', value=summoner[5])
-        
         embed.set_thumbnail(url=summoner[3])
-        
 
         await interaction.followup.send(embed=embed)
     except ValueError as e:
@@ -135,9 +116,6 @@ async def invocateur(interaction: discord.Interaction, pseudo: str, tag: str):
         await interaction.followup.send("Une erreur inattendu est survenue.")
         # Enregistrement de l'erreur pour le débogage
         print(f"Unexpected error: {e}")
-
-
-
 
 ### Maitrises ###
 def fetchMasteries(puuid, count=1):
@@ -167,9 +145,6 @@ def fetchMasteries(puuid, count=1):
         masteries.append((championIcon, championName, championLevel, championPoints))
 
     return masteries
-
-
-
 
 # Commande discord Maitrises
 @tree.command(name='maitrises', description='Meilleures Maitrises d\'un Invocateur')
@@ -206,9 +181,6 @@ async def maitrises(interaction: discord.Interaction, pseudo: str, tag: str, cou
         await interaction.followup.send("Une erreur inattendue est survenue.")
         # Enregistrement de l'erreur pour le débogage
         print(f"Unexpected error: {e}")
-
-
-    
 
 ###Game en cours###
  # Charger le fichier JSON local
@@ -249,7 +221,7 @@ def fetchGameOngoing(puuid):
         gameMode = 'Siège du Nexus'
     elif queueId == 1900:
         gameMode = 'URF'
-    elif queueId == 1710:
+    elif queueId == 1700:
         gameMode = 'Arena'
     else:
         gameMode = f'Mode non référencé: {queueId}'
@@ -264,14 +236,6 @@ def fetchGameOngoing(puuid):
 
     return None, None, None
 
-# Charger la liste des invocateurs à surveiller à partir d'un fichier JSON
-try:
-    with open('summoners_to_watch.json', 'r', encoding='utf-8') as f:
-        summoners_to_watch = json.load(f)
-except FileNotFoundError:
-    summoners_to_watch = []
-
-
 # Tâche de fond pour vérifier l'état des invocateurs
 @tasks.loop(minutes=1)
 async def check_summoners_status():
@@ -281,37 +245,29 @@ async def check_summoners_status():
             if riot_id:
                 channel = discord.utils.get(client.get_all_channels(), name='test')
                 if channel:
-                    await channel.send(f"{riot_id} est en {game_mode} et joue {champion_name}.")
+                    embed = discord.Embed(
+                    title=f"{riot_id} est en {game_mode} avec {champion_name}",
+                    color=discord.Colour.dark_embed()
+                    )
+                    await channel.send(embed=embed)
         except Exception as e:
             print(f"Error checking status for {summoner['name']}: {e}")
-
-
-# Démarrer la tâche de fond lors du démarrage du bot
-@client.event
-async def on_ready():
-    print(f"Logged in as {client.user.name}")
-    check_summoners_status.start()
-
-
-
 
 # Commande pour ajouter un invocateur à la liste
 @tree.command(name='addsummoner', description=('Ajouter un invocateur à la liste pour être notifié quand celui-ci est en game'))
 @app_commands.describe(pseudo='Nom invocateur', tag='EUW')
 async def addsummoner(interaction: discord.Interaction, pseudo: str, tag: str):
     try:
-        name, puuid = await requestSummoner(pseudo, tag)  # Assurez-vous que l'appel est attendu avec await
-        summoners_to_watch.append({'name': name, 'puuid': puuid})
+        summoner = requestSummoner(pseudo, tag)
+        summoners_to_watch.append({'name': summoner[1], 'puuid': summoner[6]})
         with open('summoners_to_watch.json', 'w', encoding='utf-8') as f:
             json.dump(summoners_to_watch, f, ensure_ascii=False, indent=4)
-        await interaction.response.send_message(f"Summoner {name} a été ajouté à la liste.")
+        await interaction.response.send_message(f"Summoner {summoner[1]} a été ajouté à la liste.")
     except ValueError as e:
         await interaction.response.send_message(f"Error: {str(e)}")
     except Exception as e:
         await interaction.response.send_message("Une erreur inattendue est survenue.")
         print(f"Unexpected error: {e}")
-
-
 
 # Commande discord Game en cours
 @tree.command(name='ingame', description='Savoir si un joueur est en jeu')
@@ -335,17 +291,13 @@ async def maitrises(interaction: discord.Interaction, pseudo: str, tag: str):
         # Enregistrement de l'erreur pour le débogage
         print(f"Unexpected error: {e}")
 
-
-
-
-
 ### Synchro ###
 @tree.command(name='sync', description='Owner Only')
 async def sync(interaction: discord.Interaction):
     idumi = os.getenv('ID_IDUMI')
-    
+
     owner_id = int(idumi)  # Assurez-vous que l'ID dans 'id.txt' est un entier et convertissez-le
-    
+
     if interaction.user.id == owner_id:
         await interaction.response.send_message('Synchronization in progress...')
         try:
@@ -359,8 +311,5 @@ async def sync(interaction: discord.Interaction):
         id = interaction.user.id
         await interaction.response.send_message(f'Seul le développeur peut utiliser cette commande -> {id} / {owner_id}')
 
-
 token = os.getenv('TOKEN_DISCORD')
-
 client.run(token=token)
-
